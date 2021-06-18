0ebe374273d771813a6c08237d8bd3ccf0c87518
LeoDog896 <LeoDog896@hotmail.com>
2021-06-18 13:53:47 -0400

    bee reader --> bbee reader

--- src/bbee_reader.rs
+++ src/bbee_reader.rs
@@ -2,10 +2,16 @@ use std::path::Path;
 use toml::Value;
 use std::error::Error;
 
+let file_name = "bbee.toml"
+
 pub fn read(current_directory: &Path) -> Result<(), Box<dyn Error>> {
 
-	let file = File::open(path.join("bbee.toml"))?;
+	let file = File::open(path.join(file_name))?;
 
 	let content = file.read_to_string(String::new())?;
 
+}
+
+pub fn exists(current_directory: &Path) -> bool {
+	return current_directory.join(file_name).exists()
 }
