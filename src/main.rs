4e504b1589775543003a02d9de67cfcf45e5a334
LeoDog896 <LeoDog896@hotmail.com>
2021-06-18 08:42:43 -0400

    Start base CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+#[structopt(about = "a buzzy build tool for the JVM.")]
+enum Cli {
+    Build
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::from_args();
 }
