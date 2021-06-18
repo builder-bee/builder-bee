864e2abbc73a73c8356347e30a9b3eaf6de83fde
LeoDog896 <LeoDog896@hotmail.com>
2021-06-18 09:04:08 -0400

    Add sample example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use structopt::StructOpt;
 
-/// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 #[structopt(about = "a buzzy build tool for the JVM.")]
-enum Cli {
+enum BeeCLI {
+    Init,
     Build
 }
 
 fn main() {
-    let cli = Cli::from_args();
+    match BeeCLI::from_args() {
+        BeeCLI::Build => {
+            println!("Building...");
+        }
+
+        BeeCLI::Init => {
+            println!("Initializing Project...");
+        }
+    }
 }
