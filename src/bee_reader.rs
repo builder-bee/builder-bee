193266945dfd2306873d6f3e2db7945745c2ee3a
LeoDog896 <LeoDog896@hotmail.com>
2021-06-18 13:50:14 -0400

    Change file name

--- src/bee_reader.rs
+++ src/bee_reader.rs
@@ -2,9 +2,9 @@ use std::path::Path;
 use toml::Value;
 use std::error::Error;
 
-pub fn read(path: &Path) -> Result<(), Box<dyn Error>> {
+pub fn read(current_directory: &Path) -> Result<(), Box<dyn Error>> {
 
-	let file = File::open(path)?;
+	let file = File::open(path.join("bbee.toml"))?;
 
 	let content = file.read_to_string(String::new())?;
 
