d9bba43e409a060a78c0c24ff8e1ae2df8d80ed0
LeoDog896 <LeoDog896@hotmail.com>
2021-06-19 16:53:29 -0400

    No more serializaiton, use manual instead

--- src/bbee_reader.rs
+++ src/bbee_reader.rs
@@ -1,32 +1,25 @@
 use std::path::Path;
 use std::fs;
-use serde::Deserialize;
+use toml::Value;
 use crate::generic_result::GenericResult;
 
 static FILE_NAME: &str = "bbee.toml";
 
-#[derive(Deserialize)]
 pub struct BBeeConfigDependency {
 	pub name: String,
 	pub version: String,
 	pub r#type: String
 }
 
-#[derive(Deserialize)]
 pub struct BBeeConfigInfo {
 	pub name: String,
 	
-	#[serde(default = "default_version")]
-	pub version: String
+	pub version: String,
 }
 
-fn default_version() -> String {
-    "1.0.0".to_string()
-}
-
-#[derive(Deserialize)]
 pub struct BBeeConfig {
-	pub info: BBeeConfigInfo
+	pub info: BBeeConfigInfo,
+	pub dependencies: Vec<BBeeConfigDependency>
 }
 
 /// Reads the bbee file and outputs a conf gstruct
@@ -36,7 +29,9 @@ pub fn read(working_directory: &Path) -> GenericResult<BBeeConfig> {
 	let config_path = working_directory.join(FILE_NAME);
 
 	// Read it using serde's serialization and TOML
-	let config: BBeeConfig = toml::from_str(&fs::read_to_string(config_path)?)?;
+	let config_toml = &fs::read_to_string(config_path)?.parse::<Value>()?;
+
+	let config = config_from_value(config_toml);
 
 	// Return the config!
 	Ok(config)
@@ -46,4 +41,19 @@ pub fn read(working_directory: &Path) -> GenericResult<BBeeConfig> {
 /// Check if the bbee config is in the project
 pub fn exists(working_directory: &Path) -> bool {
 	working_directory.join(FILE_NAME).exists()
+}
+
+fn config_from_value(value: &Value) -> BBeeConfig {
+
+	let info =  value.get("info").unwrap(); // Should always be in a bbee config.
+
+	return BBeeConfig {
+		info: BBeeConfigInfo {
+			name: info.get("name").unwrap().as_str().unwrap().to_string(),
+			version: info.get("version").unwrap_or(&Value::String("1.0.0".to_string())).as_str().unwrap_or("1.0.0").to_string(),
+		},
+		dependencies: vec![
+			BBeeConfigDependency { name: "hello".to_string(), version: "hello".to_string(), r#type: "hello".to_string() },
+		]
+	};
 }
