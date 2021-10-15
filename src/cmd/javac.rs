01e99f526fa30d088bb5ef6da009751ea634dc71
LeoDog896 <LeoDog896@hotmail.com>
2021-10-15 11:25:08 +0000

    Second attempt

--- src/cmd/javac.rs
+++ src/cmd/javac.rs
@@ -5,30 +5,36 @@ use std::path::Path;
 use std::process::Command;
 
 #[derive(Error, Debug, Clone)]
-#[error("Javac error: {output}")]
-pub struct JavaCompileError {
-	pub output: String,
+pub enum JavaCompileError {
+	#[error("Could not run javac, file: {file}, target: {target}")]
+	CanNotRun {
+		file: String,
+		target: String
+	},
+	#[error("Javac error: {output}")]
+	Failed {
+		output: String
+	}
 }
 
 /// Compiles a `file` with javac and puts it at the `target` path.
 /// For example, given `/build/classes` and `/main/src/HelloWorld.java`,
 /// It will generate a file at `/build/classes/HelloWorld.class`
 /// With the resulting compiled class.
-pub fn compile(target: &Path, file: &Path) -> Result<()> {
+pub fn compile(target: &Path, file: &Path) -> Result<(), JavaCompileError> {
 	let command_output = run(Command::new("javac")
 		.arg(file.display().to_string())
 		.arg("-d")
 		.arg(target.display().to_string()))
-		.with_context(|| format!(
-			"Can not run command javac, file: {}, target: {}",
-			file.display().to_string(),
-			target.display().to_string()
-		))?;
+		.map_err(|_| JavaCompileError::CanNotRun {
+			file: file.display().to_string(),
+			target: target.display().to_string()
+		})?;
 
 	if command_output.status.success() {
 		Ok(())
 	} else {
-		Err(anyhow!(JavaCompileError {
+		Err(anyhow!(JavaCompileError::Failed {
 			output: command_output.stderr,
 		}))
 	}
