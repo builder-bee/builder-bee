8bae3646c47cb02017f139de8e90316c01b3676a
LeoDog896 <LeoDog896@hotmail.com>
2021-10-20 11:27:48 +0000

    Add prompt to init

--- src/subcommands/init.rs
+++ src/subcommands/init.rs
@@ -2,11 +2,18 @@ use crate::config::bbee_reader;
 use anyhow::{anyhow, Result};
 use std::option::Option;
 use std::path::Path;
+use spinner::menu::{Menu,MenuOption,MenuOptional,MenuType};
 
 pub fn init(working_directory: &Path) -> Result<()> {
 	if bbee_reader::find_config(working_directory) != Option::None {
-		anyhow!("This project has already been initialized!");
+		return Err(anyhow!("This project has already been initialized!"));
 	}
+
+	let prompt = Menu::new(vec![
+		MenuOption("Project Name".into(), MenuType::Text, MenuOptional::Required, None)
+	]);
+
+	prompt.display();
 
 	// TODO
 
