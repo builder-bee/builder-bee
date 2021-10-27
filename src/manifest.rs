d90b650c8ae7375a9e4c40a7da972b155e8b05ef
LeoDog896 <LeoDog896@hotmail.com>
2021-10-27 12:03:10 +0000

    Add Manifest Documentation

--- src/manifest.rs
+++ src/manifest.rs
@@ -1,6 +1,8 @@
 use crate::config::bbee_reader::BBeeConfig;
 
 #[must_use]
+/// Generate a manifest file.
+/// Put at META-INF/MANIFEST.MF
 pub fn generate(config: &BBeeConfig) -> String {
 	format!(
 		r#"Manifest-Version: 1.0
