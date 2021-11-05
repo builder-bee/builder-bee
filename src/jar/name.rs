bc58c1a26fc709887f25a58eaafac37ad4cbfee4
LeoDog896 <LeoDog896@hotmail.com>
2021-11-05 14:07:26 +0000

    Fix up clean

--- src/jar/name.rs
+++ src/jar/name.rs
@@ -1,5 +1,6 @@
 use crate::config::bbee_reader::BBeeConfig;
 
+/// Generates a jar name from a BBeeConfig
 #[must_use]
 pub fn generate(config: &BBeeConfig) -> String {
 	format!("{}-{}.jar", config.info.name, config.info.version)
--- src/subcommands/clean.rs
+++ src/subcommands/clean.rs
@@ -12,13 +12,19 @@ pub enum CleanError {
 
 	#[error("Could not remove directory build in {0}: {1}")]
 	CouldNotRemoveDirectory(String, std::io::Error),
+
+	#[error("Could not get parent of file {0}.")]
+	CouldNotGetParentOfFile(String)
 }
 
 /// Remove the build directory from the `working_directory`
 pub fn clean(working_directory: &Path) -> Result<(), CleanError> {
 	let directory = bbee_reader::find_config(working_directory).ok_or(CleanError::NoConfigFound)?;
 
-	fs::remove_dir_all(directory.join("build")).map_err(|e| {
+	fs::remove_dir_all(
+		directory.parent()
+		.ok_or(CleanError::CouldNotGetParentOfFile(directory.display().to_string()))?.join("build")
+	).map_err(|e| {
 		CleanError::CouldNotRemoveDirectory(
 			directory
 				.join("build")
