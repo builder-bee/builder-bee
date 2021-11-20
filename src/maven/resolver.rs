2a537a2a898f737da1629751bcb9498d3bfc12bf
christian162 <christian.sheridan151@gmail.com>
2021-11-20 15:35:12 -0600

    Cleanup resolver

--- src/maven/resolver.rs
+++ src/maven/resolver.rs
@@ -20,27 +20,12 @@ pub async fn find_dependency(dependency: &Dependency, repositories: &Vec<&str>) 
             dependency.version
         );
 
-        let response_result = reqwest::get(&path).await;
+        let response = if let Ok(x) = reqwest::get(&path).await { x } else { continue; };
 
-        match response_result {
-            Ok(response) => {
-                if response.status() == StatusCode::OK {
-                    let response_url_path = response.url().path();
-                    let last_slash_index = response_url_path.rfind("/")?;
-
-                    let resulting_maven_path = &response_url_path[0..last_slash_index];
-                    let resulting_maven_path_slash_index = resulting_maven_path.rfind("/")? + 1;
-
-                    let real_maven_version = &resulting_maven_path[resulting_maven_path_slash_index..last_slash_index];
-                    let jar_file_name = format!("{}-{}.jar", dependency.artifact_id, real_maven_version);
-
-                    return Option::Some(format!("{}/{}", path, jar_file_name));
-                }       
-            },
-            Err(_) => {
-                continue;
-            }
-        };
+        if response.status() == StatusCode::OK {
+            let jar_path = format!("{}/{}-{}.jar", path, dependency.artifact_id, dependency.version);
+            return Option::Some(jar_path);
+        }
     }
 
     Option::None
