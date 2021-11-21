ce2420fb4c3caf857c242a1b6a5e684fdd878689
LeoDog896 <LeoDog896@hotmail.com>
2021-11-20 21:46:45 -0500

    Add correction methods

--- src/maven/resolver.rs
+++ src/maven/resolver.rs
@@ -6,7 +6,23 @@ pub struct Dependency {
 	pub version: String,
 }
 
-pub async fn find_dependency(dependency: &Dependency, repositories: &Vec<&str>) -> Option<String> {
+/// Removes a hanging slash from the end of a repository URL.
+#[must_use]
+pub fn correct_repository(url: &str) -> String {
+	if url.ends_with('/') {
+		url[..url.len() - 1].to_string()
+	} else {
+		url.to_owned()
+	}
+}
+
+#[must_use]
+pub fn correct_repositories(repositores: &Vec<String>) -> Vec<String> {
+	repositores.iter().map(|repository| correct_repository(repository)).collect::<Vec<String>>()
+}
+
+
+pub async fn find_dependency(dependency: &Dependency, repositories: &Vec<String>) -> Option<String> {
 	let mut iter = repositories.iter();
 
 	while let Some(repository) = iter.next() {
--- tests/dependency_test.rs
+++ tests/dependency_test.rs
@@ -3,7 +3,7 @@
 mod dependency_test {
 
 	use anyhow::Result;
-	use bbee::maven::resolver::{Dependency, find_dependency};
+	use bbee::maven::resolver::{Dependency, find_dependency, correct_repositories};
 	use pretty_assertions::assert_eq;
 
 	#[tokio::test]
@@ -14,7 +14,7 @@ mod dependency_test {
 			version: String::from("master-SNAPSHOT")
 		};
 		
-		let respositories = vec!["https://jitpack.io/"];
+		let respositories = correct_repositories(vec!["https://jitpack.io/"]);
 		let result = find_dependency(&dependency, &respositories).await.ok_or_else(|| anyhow::anyhow!("No result"))?;
 
 		assert_eq!(result, "https://jitpack.io/com/github/Minestom/Minestom/master-SNAPSHOT/Minestom-master-SNAPSHOT.jar");
